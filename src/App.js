c20558233a671d354b0b4f88be61aba7eed15eb4
ncj1997 <nishshankacj@gmail.com>
2023-12-29 22:49:20 +1100

    app update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { useState, useEffect } from 'react';
 function App() {
-  const [data, setData] = useState('')
+  const [data, setData] = useState([])
   const [age, setAge] = useState('25')
   const [name, setName] = useState('Nish')
   const getData = async () => {
@@ -22,12 +22,10 @@ function App() {
       <h3>Hello my name is {name} age is {age} </h3>
       <button className='btn btn-outline-danger' onClick={() => setAge('100')}>Click for Age</button>
       <button onClick={() => setName('Charm')}>Click for Name</button>
-   
+      {data.map((item, index) => (
+          <li key={index}>{JSON.stringify(item.title)}</li>
+        ))}
 
-      <div class="d-grid gap-5 d-md-block">
-        <button class="btn btn-primary" type="button">Button</button>
-        <button class="btn btn-primary" type="button">Button</button>
-      </div>
     </div>
   );
 }
